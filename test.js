94818e1ccb3980fee515a13384a6304503a1cee7
enzovonkunoschy@gmail.com <enzovonkunoschy@gmail.com>
2024-08-23 07:53:51 -0300

    Primer commit.  Crear clase usuario.

--- test.js
+++ test.js
@@ -2,32 +2,21 @@ const Clases = require('./clases.js');
 const Helper = require('./helper.js');
 const Modelo = require('./modelo.js');
 
-function testPerfil() {
-    console.log("Perfil ------------------");
-    const perfil = new Clases.Perfil();
-    perfil.addCasoUso("cu00");
-    console.log(perfil);
-    perfil.addCasoUso("cu01");
-    console.log(perfil);
-    perfil.delCasoUso("cu00");
-    console.log(perfil);
-    return perfil
+function testUsuario(){
+    const usu = new Clases.Usuario("Enzo","1234","miRol")
+    console.log(usu.getNombre()=="Enzo" &&
+        usu.getContrasena() == "1233" &&
+        usu.getRol("miRol")
+        )
+    usu.setNombre("Laura")
+    usu.setContrasena("4321")
+    usu.setRol("otroRol")
+
+    console.log(usu.getNombre == "Laura" &&
+        usu.getContrasena == "4321" &&
+        usu.getRol == "otroRol"
+    )
+
 }
 
-function tetsUsuario() {
-    console.log("Usuario --------------------")
-    const unUsuario = new Clases.Usuario("Enzo Perez","enzito","1234",Helper.getUuid(),testPerfil());
-    console.log(unUsuario);
-    let str_unUsuario = JSON.stringify(unUsuario);
-    console.log(str_unUsuario);
-    let par_unUsuario = JSON.parse(str_unUsuario);
-    console.log(par_unUsuario);
-    let obj_unUsuario = Clases.Usuario.fromJSON(par_unUsuario);
-    console.log(obj_unUsuario);
-}
-
-//Modelo.guardarUsuario(unUsuario);
-
-//console.log("getUsuarios ---------------------")
-//console.log(Modelo.getUsuarios());
-
+testUsuario()
