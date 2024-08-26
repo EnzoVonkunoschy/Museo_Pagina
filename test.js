78425e1750d33695370e9cda773a23c1a3881873
enzovonkunoschy@gmail.com <enzovonkunoschy@gmail.com>
2024-08-26 18:09:28 -0300

    Se debe agregar Modelo.eliminarUsuario(...)

--- test.js
+++ test.js
@@ -2,6 +2,25 @@ const Clases = require('./clases.js');
 const Helper = require('./helper.js');
 const Modelo = require('./modelo.js');
 
+
+function testUsuarios(){
+    console.log("testUsuarios ------------------------------")
+    const nuevoUsuario = new Clases.Usuario("Usuario Test","Contrasena Test","Rol test")
+    
+    Modelo.guardarUsuario(nuevoUsuario)
+    let todosLosUsuarios = Modelo.getUsuarios()
+    let filUsuarios = todosLosUsuarios.filter(x=>x.getNombre() == "Usuario Test" &&
+                                                x.getContrasena() == "Contrasena Test" &&
+                                                x.getRol() == "Rol test")
+   console.log(filUsuarios.length == 1?true:false)
+
+   Modelo.eliminarUsuario(nuevoUsuario.nombre)
+   todosLosUsuarios = Modelo.getUsuarios()
+   filUsuarios = todosLosUsuarios.filter(x=>x.getNombre() == "Usuario Test")
+   console.log(filUsuarios.length == 0 ? true : false)
+
+}
+
 function testUsuario(){
     const usu = new Clases.Usuario("Enzo","1234","miRol")
     console.log(usu.getNombre()=="Enzo" &&
@@ -20,3 +39,5 @@ function testUsuario(){
 }
 
 testUsuario()
+testNoticia()
+testUsuarios()
